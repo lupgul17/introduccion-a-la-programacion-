7c59a613dc7050e5745b9795206a8fe117908f2e
Luis Gudiel <luisgudiel1711@gmail.com>
2023-03-11 16:15:02 -0600

    cambios final

--- Tarea_fechas/Tarea_fechas/Program.cs
+++ Tarea_fechas/Tarea_fechas/Program.cs
@@ -1,68 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-#region variables 
-int diaActual;
-int diaNacimiento;
-int mesActual;
-int mesNacimiento;
-int annioActual;
-int annioNacimiento;
-bool verificador=false;
-#endregion
-#region Algoritmo
-Console.WriteLine("Ingrese el dia que nacio....");
-diaNacimiento = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ingrese el mes que nacio....");
-mesNacimiento = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ingrese el annio que nacio....");
-annioNacimiento = Convert.ToInt32(Console.ReadLine());
-//fecha actual por medio de DateTime
-diaActual = DateTime.Now.Day;
-mesActual = DateTime.Now.Month;
-annioActual = DateTime.Now.Year;
-Console.WriteLine(diaActual +"-"+mesActual +"-"+annioActual);
-//Algoritmo condicional 
-if(annioActual-annioNacimiento > 18)
+﻿
+internal class Program
 {
-    verificador = true;    
-}
-else
-{
-    if(annioActual - annioNacimiento <= 16)
+    private static void Main(string[] args)
     {
-        verificador=false;    
-    }
-    else if(annioActual - annioNacimiento >= 17)
-    {
-        if(mesActual > mesNacimiento)
+        #region variables 
+        int diaActual;
+        int diaNacimiento;
+        int mesActual;
+        int mesNacimiento;
+        int annioActual;
+        int annioNacimiento;
+        bool verificador = false;
+        #endregion
+        #region Algoritmo
+        Console.WriteLine("Ingrese el dia que nacio....");
+        diaNacimiento = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el mes que nacio....");
+        mesNacimiento = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese el annio que nacio....");
+        annioNacimiento = Convert.ToInt32(Console.ReadLine());
+        //fecha actual por medio de DateTime
+        diaActual = DateTime.Now.Day;
+        mesActual = DateTime.Now.Month;
+        annioActual = DateTime.Now.Year;
+        Console.WriteLine(diaActual + "-" + mesActual + "-" + annioActual);
+        //Algoritmo condicional 
+        if (annioActual - annioNacimiento > 18)
         {
-            verificador=true;
+            verificador = true;
         }
-        if(mesActual < mesNacimiento)
+        else
         {
-            verificador = false;
-        }
-        else if(mesActual== mesNacimiento) 
-        {
-            if(diaActual > diaNacimiento) 
+            if (annioActual - annioNacimiento <= 16)
             {
                 verificador = false;
             }
-            else if(diaActual<=diaNacimiento)
+            else if (annioActual - annioNacimiento >= 17)
             {
-                verificador =true;
+                if (mesActual > mesNacimiento)
+                {
+                    verificador = true;
+                }
+                if (mesActual < mesNacimiento)
+                {
+                    verificador = false;
+                }
+                else if (mesActual == mesNacimiento)
+                {
+                    if (diaActual > diaNacimiento)
+                    {
+                        verificador = false;
+                    }
+                    else if (diaActual <= diaNacimiento)
+                    {
+                        verificador = true;
+                    }
+                }
             }
+        }
+        if (verificador == true)
+        {
+            Console.WriteLine("Es mayor de edad");
+        }
+        else
+        {
+            Console.WriteLine("Es menor de edad");
         }
     }
 }
-if(verificador==true)
-{
-    Console.WriteLine("Es mayor de edad");
-}
-else
-{
-    Console.WriteLine("Es menor de edad");
-}
+
+#endregion
+#region Algoritmo
 #endregion
 
 
